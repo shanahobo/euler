f5a05c084bd3ef9673e0547e94a0c9bb098bda42
Niall <www.niall@gmail.com>
2015-07-14 12:40:27 +0100

    remove code from MergeArr

--- euler/src/MergeArr.java
+++ euler/src/MergeArr.java
@@ -2,23 +2,6 @@
 
 public class MergeArr{
 	
-    public static void main(String[] args) {
-    	
-     	
-    	String [] words = {"cat", "cvic"};
-    	
-    	for(int i = 0; i<words.length; i++){
-             
-             
-             String [] chars = words[i].split("(?!^)");
-             
-             for(int x = 0 ; x < chars.length; x++){
-            	 System.out.println(chars[x]);
-             }
-             
-             
-         }
-    
-    }
+
 
 }
